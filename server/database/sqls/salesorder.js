89776cf3ba5e99483b1a43c7890fd32923367acc
KwonDongIk <dongikik@kakao.com>
2025-05-02 09:27:42 +0900

    0502 AM 09:27 commit

--- server/database/sqls/salesorder.js
+++ server/database/sqls/salesorder.js
@@ -39,6 +39,86 @@ JOIN employees AS e
 WHERE so.delivery_date = ? or so.sorder_code = ? or c.client_name = ? or i.item_name = ? or c.client_mgr = ?
 ORDER BY so.sorder_code DESC`;
 
+// 주문서 조회 => 납기일자로 조회
+const selectSalesOrderByDeliveryDate =
+`SELECT
+  so.sorder_code,
+  c.client_name,
+  c.client_mgr,
+  i.item_name,
+  so.delivery_date,
+  so.sorder_count,
+  so.status,
+  e.emp_id
+FROM salesorder AS so
+JOIN client     AS c
+  ON so.client_code = c.client_code
+JOIN items      AS i
+  ON so.item_code   = i.item_code
+JOIN employees AS e
+  ON so.emp_id = e.emp_id
+ORDER BY so.delivery_date DESC`;
+
+// 주문서 조회 => 주문번호로 조회
+const selectSalesOrderBySorderCode =
+`SELECT
+  so.sorder_code,
+  c.client_name,
+  c.client_mgr,
+  i.item_name,
+  so.delivery_date,
+  so.sorder_count,
+  so.status,
+  e.emp_id
+FROM salesorder AS so
+JOIN client     AS c
+  ON so.client_code = c.client_code
+JOIN items      AS i
+  ON so.item_code   = i.item_code
+JOIN employees AS e
+  ON so.emp_id = e.emp_id
+ORDER BY so.sorder_code DESC`;
+
+// 주문서 조회 => 거래처명 조회
+const selectSalesOrderByClientName =
+`SELECT
+  so.sorder_code,
+  c.client_name,
+  c.client_mgr,
+  i.item_name,
+  so.delivery_date,
+  so.sorder_count,
+  so.status,
+  e.emp_id
+FROM salesorder AS so
+JOIN client     AS c
+  ON so.client_code = c.client_code
+JOIN items      AS i
+  ON so.item_code   = i.item_code
+JOIN employees AS e
+  ON so.emp_id = e.emp_id
+ORDER BY c.client_name DESC`;
+
+// 주문서 조회 => 품목명 조회
+const selectSalesOrderByItemName =
+`SELECT
+  so.sorder_code,
+  c.client_name,
+  c.client_mgr,
+  i.item_name,
+  so.delivery_date,
+  so.sorder_count,
+  so.status,
+  e.emp_id
+FROM salesorder AS so
+JOIN client     AS c
+  ON so.client_code = c.client_code
+JOIN items      AS i
+  ON so.item_code   = i.item_code
+JOIN employees AS e
+  ON so.emp_id = e.emp_id
+ORDER BY i.item_name DESC`;
+
 // 주문서 수정
 const updateSalesOrder =
 `UPDATE salesorder AS so
