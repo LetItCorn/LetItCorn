25c11e1038622bd2eb27995bbfcdcd3ff82d8eb7
Archi <dnrdl6817@gmail.com>
2025-05-08 00:39:24 +0900

    home commit

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -11,7 +11,6 @@ import Profile from "../views/Profile.vue";
 import SignIn from "../views/SignIn.vue";
 import SignUp from "../views/SignUp.vue";
 import Leetest from "../views/Leetest.vue";
-import Process from "@/views/Process.vue";
 import MReturnForm   from '../views/MReturnForm.vue';
 import MReturnsList  from '../views/MReturnsList.vue';
 import Productionplan from "@/views/Productionplan_v2.vue";
@@ -25,12 +24,12 @@ import ProductionPlanInquiry from '@/views/ProductionPlanInquiry.vue';
 import MOrderForm from '../views/MOrderForm.vue';
 import MOrdersList from '../views/MOrdersList.vue';
 import Bom from '@/views/Bom.vue';
-import MOutboundForm from '../views/MOutboundForm.vue';
 // import MOutboundList from '../views/MOutboundList.vue';
 import MMovement       from '../views/MMovement.vue';
 import QCHistory from '../views/QCHistory.vue';
 import MOutboundForm from '../views/MOutboundForm.vue';
-import ProductionPlanInquiry from '@/views/ProductionPlanInquiry.vue';
+import Process from "@/views/process.vue";
+import Material from "@/views/Material.vue";
 
 
 const routes = [
@@ -193,6 +192,11 @@ const routes = [
     name: 'QCHistory',
     component: QCHistory
   },
+  {
+    path: '/process',
+    name:'process',
+    component: Process
+  }
 ];
 
 const router = createRouter({
