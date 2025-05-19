92d615c38281a71249d378314eb7bea277f5057c
Archi <dnrdl6817@gmail.com>
2025-05-19 15:55:39 +0900

    test3

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
   require('dotenv').config({path : './database/configs/dbConfig.env'});
+  const path = require ('path')
   const express = require('express');
   const app = express();
   const session = require('express-session');
